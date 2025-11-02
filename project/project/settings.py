8ea04e39488bdb403cdd3f59b254bf6f267e9f80
Arsen-kbtu <122906048+Arsen-kbtu@users.noreply.github.com>
2025-11-02 19:46:24 +0500

    Revert "Practice 6 main duplicate 1"

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'abstract',
-    'players',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = 'djangorlar.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = 'djangorlar.wsgi.application'
 
 
 # Database
